5af595a5f5ef9b99702e1f2ded718e88c8b7edc1
vorozhba_sv <s.vorozhba@gmail.com>
2017-09-12 15:22:11 +0300

    controllers, hrlper, views fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   # Настройка для работы девайза при правке профиля юзера
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # Хелпер метод, доступный во вьюхах
+  helper_method :current_user_can_edit?
+
   # Настройка для девайза — разрешаем обновлять профиль, но обрезаем
   # параметры, связанные со сменой пароля.
   def configure_permitted_parameters
@@ -11,5 +14,11 @@ class ApplicationController < ActionController::Base
       :account_update,
       keys: [:password, :password_confirmation, :current_password]
     )
+  end
+
+  # Вспомогательный метод, возвращает true, если текущий залогиненный юзер
+  # может править указанное событие
+  def current_user_can_edit?(event)
+    user_signed_in? && event.user == current_user
   end
 end
